986c26d256bce49095abed95458a34b128e3beb5
javigr87 <53544798+javigr87@users.noreply.github.com>
2019-08-07 23:23:57 +0200

    Add files via upload

---
+++ NVL-JS-P6a/practica1.js
@@ -0,0 +1,6 @@
+    var nombre= "Javier García Ruizolalla";
+    var edad=31;
+    var dias="lunes, martes, miércoles, jueves, viernes, sábado, domingo";
+    var gravedad= 9.8 m/s2;
+    var impares= [1,3,5];
+    
